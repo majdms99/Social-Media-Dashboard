9c43435e81c4d9bfbc1cd19010f3b064bf2fece6
msalami <msalami@solutions-integration.com>
2022-08-31 20:33:18 +0300

    upload files0

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='pt-16 pb-8 bg-white  dark:bg-darkbg-dark text-center'>
-            <h3 className='text-sm font-semibold text-gray-400'>Copyright 2022 | Coded by ❤ <a href="https://majd-salameh.netlify.app/" target='_blank'>Majd Salameh</a>.</h3>
+            <h3 className='text-sm font-semibold text-gray-400'>Copyright 2022 | Coded by ❤ <a href="https://majd-salameh.netlify.app/" >Majd Salameh</a>.</h3>
         </div>
     )
 }
